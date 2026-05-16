f5360309b7d45d3c861cf79a1d3a6d7c03db5320
Ankit Mishra <ankvats21@gmail.com>
2026-05-16 23:12:56 +0530

    refactor: remove deprecated WifiManager and Tca9555Manager files after HAL restructuring

--- main/hal/Tca9555Manager.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-// DEPRECATED: This class has been renamed IoExpander and moved to
-// hal/io/IoExpander.h as part of the HAL restructuring (Phase 1).
-// Please update your includes to: #include "hal/io/IoExpander.h"
-#include "hal/io/IoExpander.h"
--- main/hal/WifiManager.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-// DEPRECATED: This file has been moved to hal/network/WifiManager.h
-// as part of the HAL restructuring (Phase 3).
-// Please update your includes to: #include "hal/network/WifiManager.h"
-#include "hal/network/WifiManager.h"
